7027764f79d45543b3973752e3f39e208ec0f7e2
root <misha2012gor@gmail.com>
2025-10-12 16:10:35 +0300

    TickShiftModule 200

--- src/main/java/net/shoreline/client/impl/module/movement/TickShiftModule.java
+++ src/main/java/net/shoreline/client/impl/module/movement/TickShiftModule.java
@@ -16,16 +16,13 @@ import net.shoreline.client.util.player.MovementUtil;
  */
 public class TickShiftModule extends ToggleModule {
     // Basically auto timer for NCP
-    //
+
     Config<Integer> ticksConfig = new NumberConfig<>("MaxTicks", "Maximum charge ticks", 1, 20, 200);
     Config<Integer> packetsConfig = new NumberConfig<>("Packets", "Packets to release from storage every tick", 1, 1, 5);
     Config<Integer> chargeSpeedConfig = new NumberConfig<>("ChargeSpeed", "The speed to charge the stored packets", 1, 1, 5);
-    //
+
     private int packets;
 
-    /**
-     *
-     */
     public TickShiftModule() {
         super("TickShift", "Exploits NCP to speed up ticks",
                 ModuleCategory.MOVEMENT);
